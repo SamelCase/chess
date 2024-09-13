355c2576173e0b1a6e3acb0db0a1fdf4d401bd49
SamelCase <samul.geary@gmail.com>
2024-09-13 17:38:50 -0600

    Progress on Chess Move

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -7,25 +7,27 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
-
+    private final ChessPosition start;
+    private final ChessPosition end;
+    private final ChessPiece.PieceType promotionPiece;
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.start = startPosition;
+        this.end = endPosition;
+        this.promotionPiece = promotionPiece;
     }
-
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return start;
     }
-
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return end;
     }
-
     /**
      * Gets the type of piece to promote a pawn to if pawn promotion is part of this
      * chess move
@@ -33,6 +35,18 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotionPiece;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove move = (ChessMove) o;
+        return (start.equals(move.start) && end.equals(move.end) && promotionPiece == move.promotionPiece);
+    }
+    @Override
+    public int hashCode() {
+        var promotionCode = (promotionPiece == null ? 9: promotionPiece.ordinal());
+        return (71*start.hashCode()) + end.hashCode() + promotionCode;
     }
 }
