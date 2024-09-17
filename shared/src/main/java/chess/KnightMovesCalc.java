2e64f79db1aea12de970586c11d431cd95860e4c
SamelCase <samul.geary@gmail.com>
2024-09-17 10:31:26 -0600

    Add pieceType moves calc

--- shared/src/main/java/chess/KnightMovesCalc.java
+++ shared/src/main/java/chess/KnightMovesCalc.java
@@ -1,4 +1,16 @@
 package chess;
 
+import java.util.Collection;
+
 public class KnightMovesCalc {
+    public static void pieceMoves(ChessBoard board, ChessPosition position, Collection<ChessMove> validMoves){
+        MovesCalculator.movX(board,position,validMoves,2,1,true);
+        MovesCalculator.movX(board,position,validMoves,2,-1,true);
+        MovesCalculator.movX(board,position,validMoves,-2,-1,true);
+        MovesCalculator.movX(board,position,validMoves,-2,1,true);
+        MovesCalculator.movX(board,position,validMoves,1,2,true);
+        MovesCalculator.movX(board,position,validMoves,1,-2,true);
+        MovesCalculator.movX(board,position,validMoves,-1,-2,true);
+        MovesCalculator.movX(board,position,validMoves,-1,2,true);
+    }
 }
--- shared/src/main/java/chess/QueenMovesCalc.java
+++ shared/src/main/java/chess/QueenMovesCalc.java
@@ -1,4 +1,16 @@
 package chess;
 
+import java.util.Collection;
+
 public class QueenMovesCalc {
+    public static void pieceMoves(ChessBoard board, ChessPosition position, Collection<ChessMove> validMoves) {
+        MovesCalculator.movX(board, position, validMoves, 1, 1, true);
+        MovesCalculator.movX(board, position, validMoves, 1, -1, true);
+        MovesCalculator.movX(board, position, validMoves, -1, -1, true);
+        MovesCalculator.movX(board, position, validMoves, -1, 1, true);
+        MovesCalculator.movX(board, position, validMoves, 0, 1, true);
+        MovesCalculator.movX(board, position, validMoves, 0, -1, true);
+        MovesCalculator.movX(board, position, validMoves, -1, 0, true);
+        MovesCalculator.movX(board, position, validMoves, 1, 0, true);
+    }
 }
