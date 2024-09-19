8977402797b8c28a3812ffb395081592e19f2569
SamelCase <samul.geary@gmail.com>
2024-09-18 20:34:39 -0600

    Passed King,Queen,Rook,Knight moves tests

--- shared/src/main/java/chess/PawnMovesCalc.java
+++ shared/src/main/java/chess/PawnMovesCalc.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 
 public class PawnMovesCalc {
     public static void pieceMoves(ChessBoard board, ChessPosition position, Collection<ChessMove> validMoves){
-        int[][] pawnMoves = { {1,1}, {1,-1}, {-1,1}, {-1,-1}, {0,1},{1,0},{-1,0},{1,0} };
+        int[][] pawnMoves = { {1,1}, {1,-1}, {-1,1}, {-1,-1}, {1,0}, {-1,0}};
         MovesCalculator.movX(board,position,validMoves,pawnMoves,true);
     }
 }
