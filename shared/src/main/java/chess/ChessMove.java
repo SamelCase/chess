d3a4fcf244291741eeddb8069d91bf3f55f0d377
SamelCase <samul.geary@gmail.com>
2024-10-14 20:29:38 -0600

    DONE

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,11 +1,4 @@
 package chess;
-
-/**
- * Represents moving a chess piece on a chessboard
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
- */
 public class ChessMove {
     private final ChessPosition start;
     private final ChessPosition end;
@@ -16,24 +9,12 @@ public class ChessMove {
         this.end = endPosition;
         this.promotionPiece = promotionPiece;
     }
-    /**
-     * @return ChessPosition of starting location
-     */
     public ChessPosition getStartPosition() {
         return start;
     }
-    /**
-     * @return ChessPosition of ending location
-     */
     public ChessPosition getEndPosition() {
         return end;
     }
-    /**
-     * Gets the type of piece to promote a pawn to if pawn promotion is part of this
-     * chess move
-     *
-     * @return Type of piece to promote a pawn to, or null if no promotion
-     */
     public ChessPiece.PieceType getPromotionPiece() {
         return promotionPiece;
     }
@@ -49,7 +30,6 @@ public class ChessMove {
         var promotionCode = (promotionPiece == null ? 9: promotionPiece.ordinal());
         return (71*start.hashCode()) + end.hashCode() + promotionCode;
     }
-
     @Override
     public String toString() {
         return "ChessMove{" +
