a9c1f305d9c2e9abab703d4e67bcd883207095cc
SamelCase <samul.geary@gmail.com>
2024-10-28 18:37:54 -0600

    ADD Begun implementing unit tests

---
+++ server/src/test/java/service/GameServiceTests.java
@@ -0,0 +1,19 @@
+package service;
+import dataaccess.DataAccess;
+import dataaccess.MemDataAccess;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+    private UserService userService;
+    private DataAccess dataAccess;
+
+    @BeforeEach
+    void setUp() {
+        dataAccess = new MemDataAccess();
+        userService = new UserService(dataAccess);
+    }
+
+    // Test methods will go here
+}
